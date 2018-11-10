7f67d08fe03be5c2089944b625d8c9308eee0081
Pablo Maceda <pablomaceda@Pablos-MacBook-Pro.local>
2018-11-10 12:24:43 -0600

    Completed Homework
    Completed the homework for python_challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,16 +1,76 @@
+
+#Import csv and os modules for additional functionality.
+
 import csv
 import os
 
-csvpath_bank = os.path.join('..', 'UTAUS201810DATA2', 'Python', 
-                        'Homework', 'Instructions', 'PyBank', 
-                        'Resources', 'budget_data.csv')
+# Path to relevant data file. 
+csvpath_bank = os.path.join('Resources', 'budget_data.csv')
 
+# Open csv file and set to read. Delimiter is ',' because
+# they're comma-separated values.
 with open(csvpath_bank, newline='') as csvfile_bank:
     csvreader_bank = csv.reader(csvfile_bank, delimiter=',')
 
+    # Has header. `next()` is a function that skips a row
+    # for an iterable. 
     csv_header_bank = next(csvfile_bank)
     print(f"Header: {csv_header_bank}")
 
+
+    # Initialize variables used in for loop. 
+    # Names should describe what variables hold. 
+    count_row_months = 0
+    net_profits_losses = 0
+    agg_bw_months = 0
+    avg_bw_months = 0
+    prev_month = 0 
+    agg_bw_months_check = 0
+    greatest_decrease = 0
+    greatest_increase = 0
+
+    # `for` loop iterates through rows
     for row in csvreader_bank:
-        print(row)
+        count_row_months += 1
+        
+        profits_losses = int(row[1])
+        net_profits_losses += profits_losses
+
+        curr_month = row[1]
+        change_bw_months = int(curr_month) - int(prev_month)
+        avg_bw_months = (change_bw_months)/2
+
+        agg_bw_months += avg_bw_months
+        prev_month = curr_month
+
+        if greatest_decrease > change_bw_months:
+            greatest_decrease = change_bw_months
+            greatest_decrease_date = row[0]
+        elif greatest_increase < change_bw_months:
+            greatest_increase = change_bw_months
+            greatest_increase_date = row[0]
+
+
+
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {count_row_months}")
+print(f"Total Net Profits/Losses: ${net_profits_losses}")
+print(f"Average Change Between Months: ${agg_bw_months}")
+print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})")
+
+output_path = os.path.join("financial_analysis.csv")
+
+with open(output_path, "w", newline='') as financial_analysis:
+    csvwriter = csv.writer(financial_analysis)
+    csvwriter.writerow("Financial Analysis")
+    csvwriter.writerow("----------------------------")
+    csvwriter.writerow(f"Total Months: {count_row_months}")
+    csvwriter.writerow(f"Total Net Profits/Losses: ${net_profits_losses}")
+    csvwriter.writerow(f"Average Change Between Months: ${agg_bw_months}")
+    csvwriter.writerow(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})")
+    csvwriter.writerow(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})")
+
+
 
---
+++ PyPoll/main.py
@@ -0,0 +1,86 @@
+
+
+import os
+import csv
+import operator
+
+csvpath_poll = os.path.join('Resources', 'election_data.csv')
+
+with open(csvpath_poll, newline='') as csvfile_poll:
+    csvreader_poll = csv.reader(csvfile_poll, delimiter=',')
+
+
+    csvheader_poll = next(csvfile_poll)
+    print(f"Header: {csvheader_poll}")
+
+    total_votes = 0
+    tally_1 = 0
+    tally_2 = 0
+    tally_3 = 0
+    tally_4 = 0
+    candidate_names = []
+    results = {}
+
+    for row in csvreader_poll:
+
+        total_votes += 1
+
+        if row[2] not in candidate_names:
+            candidate_names.append(row[2])
+    
+
+        if candidate_names[0] == row[2]:
+            tally_1 += 1
+        elif candidate_names[1] ==row[2]:
+            tally_2 += 1
+        elif candidate_names[2] == row[2]:
+            tally_3 += 1
+        else:
+            tally_4 += 1
+
+
+    print(tally_1)
+    print(tally_2)
+    print(tally_3)
+    print(tally_4) 
+
+    list_tallies = [tally_1, tally_2, tally_3, tally_4]
+
+    final_results = dict(zip(candidate_names, list_tallies))
+    print(final_results)
+    winner = max(final_results.items(), key=operator.itemgetter(1))[0]   
+        
+    percent_1 = round(tally_1/total_votes * 100, 3)
+    percent_2 = round(tally_2/total_votes * 100, 3)
+    percent_3 = round(tally_3/total_votes * 100, 3)
+    percent_4 = round(tally_4/total_votes * 100, 3)
+
+
+
+
+
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {total_votes}")
+print("-------------------------")
+print(f"{candidate_names[0]}: {percent_1}% ({tally_1})")
+print(f"{candidate_names[1]}: {percent_2}% ({tally_2})")
+print(f"{candidate_names[2]}: {percent_3}% ({tally_3})")
+print(f"{candidate_names[3]}: {percent_4}% ({tally_4})")
+print("-------------------------")
+print(f"Winner: {winner}")
+print("-------------------------")
+
+with open(output_path, "w", newline='') as election_results:
+    csvwriter = csv.writer(election_results)
+    csv.writerow("Election Results")
+    csv.writerow("-------------------------")
+    csv.writerow(f"Total Votes: {total_votes}")
+    csv.writerow("-------------------------")
+    csv.writerow(f"{candidate_names[0]}: {percent_1}% ({tally_1})")
+    csv.writerow(f"{candidate_names[1]}: {percent_2}% ({tally_2})")
+    csv.writerow(f"{candidate_names[2]}: {percent_3}% ({tally_3})")
+    csv.writerow(f"{candidate_names[3]}: {percent_4}% ({tally_4})")
+    csv.writerow("-------------------------")
+    csv.writerow(f"Winner: {winner}")
+    csv.writerow("-------------------------")
